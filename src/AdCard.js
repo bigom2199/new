ba99606cf13555fe9ada3bbd9581f971b69ac7cb
Bigom <bigom2199@gmail.com>
2023-12-13 19:34:07 +0100

    all new project

---
+++ src/AdCard.js
@@ -0,0 +1,30 @@
+
+import React from "react";
+function AdCard({ advertisement, buttonClickHandler }) {
+    return (
+      <div className="card mb-4 col-md-4">
+        <img
+          className="card-img-top"
+          src={advertisement.image}
+          alt={advertisement.title}
+        />
+        <div className="card-body">
+          <h5 className="card-title">{advertisement.title}</h5>
+          <p className="card-text">{advertisement.description}</p>
+          <p className="card-text">Contact: {advertisement.contact}</p>
+        </div>
+        <div className="card-footer text-center">
+          <button
+            className="btn btn-info btn-block"
+            onClick={() => {
+              buttonClickHandler(advertisement.contact);
+            }}
+          >
+            Details
+          </button>
+        </div>
+      </div>
+    );
+  }
+  
+  export default AdCard;
---
+++ src/Components/AdList.js
@@ -0,0 +1,23 @@
+import AdCard from "../AdCard";
+
+function AdList({ advertisements }) {
+    const handleButtonClick = (input) => {
+      alert("Contact:" + input);
+    };
+    return (
+      <div id="advertisements" className="row m-5">
+        <div className="m-2">
+          <h3>Advertisement List</h3>
+        </div>
+        {advertisements.map((advertisement) => (
+          <AdCard
+            key={advertisement.title}
+            advertisement={advertisement}
+            buttonClickHandler={handleButtonClick}
+          />
+        ))}
+      </div>
+    );
+  }
+  
+  export default AdList;
