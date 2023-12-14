c687e5fab1d9b8cf008f90b99220bf68ffd34846
Bigom <bigom2199@gmail.com>
2023-12-14 09:53:41 +0100

    Card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,32 +1,41 @@
-import{react} from"./domutils.js";
+import{createElementHelper, react} from"./domutils.js";
 
 
-const render = ({ target, title, description, image, contact }) => {
-    const mainDiv = createElementHelper("div", "", ["col", "mb-4"]);
-    const cardDiv = createElementHelper("div", "", ["card"]);
-    mainDiv.appendChild(cardDiv);
-    const img = createElementHelper("img");
-    img.src = image;
-    img.alt = title;
+const  render = ({target,title,image,contact,description}) => {
+    const mainDiv = createElementHelper('div','',['col','mb-4']);
+    const cardDiv = createElementHelper('div','',['card']);
+
+    mainDiv.apprndChild(cardDiv);
+    const img= createElementHelper('img');
+    img.src=image;
+
+    img.alt=title;
     cardDiv.appendChild(img);
 
-    const cardBodyDiv = createElementHelper("div", "", ["card-body"]);
-    const cardTitle = createElementHelper("h5", title, ["card-title", "fw-bold"]);
+    const cardBodyDiv= createElementHelper('div','',['card-body']);
+
+    const cardTitle= createElementHelper('h5',title,['card-title','fw-bold']);
     cardBodyDiv.appendChild(cardTitle);
-    const cardText = createElementHelper("p", description, ["card-text"]);
+
+    const cardText = createElementHelper('p',description,['card-text']);
     cardBodyDiv.appendChild(cardText);
-    const cardButton = createElementHelper("button", "Details", ["btn", "btn-info", "shadow", "w-100"]);
-    const contactDetails = createElementHelper("p", "Contact: ***", ["card-text"]);
+    
+    const cardButton = createElementHelper('button', 'details', ['btn', 'btn-info', 'shadow', 'w-100']);
+          const contactDetails = createElementHelper  ('p','contact: ',['card-trxt']);       
+        
+
     contactDetails.addEventListener('click', (e) => {
-        if (e.target.innerText.includes("***")) {
+        if (e.target.innerText.includes('')) {
             e.target.innerText = `Contact: ${contact}`;
         } else {
-            e.target.innerText = "Contact: ***";
+            e.target.innerText = 'contact: ';
         }
     });
-    cardBodyDiv.appendChild(contactDetails);
 
-    cardButton.addEventListener('click', () => alert("Contact: " + contact));
+
+    cardBodyDiv.appendChild(contactDetails);
+    
+    cardButton.addEventListener('click', () => alert('Contact: ' + contact));
     cardBodyDiv.appendChild(cardButton);
 
     cardDiv.appendChild(cardBodyDiv);
